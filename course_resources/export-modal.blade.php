407bae3524fef2b43b738a80c1dc724271340cf0
Alex Garrett-Smith <alex@codecourse.com>
2025-03-04 13:30:13 +0000

    course_resources

---
+++ course_resources/export-modal.blade.php
@@ -0,0 +1,14 @@
+<div class="p-8">
+    <div class="sm:flex sm:items-start">
+        <div class="mt-3 text-center sm:mt-0 sm:text-left">
+            <h3 class="text-base font-semibold text-gray-900" id="modal-title">Export records</h3>
+            <div class="mt-2">
+                <p class="text-sm text-gray-500">You're about to export <strong>x records</strong>. We'll send you an email when the export is available.</p>
+            </div>
+        </div>
+    </div>
+    <div class="mt-5 sm:mt-4 sm:flex sm:flex-row-reverse gap-2">
+        <x-primary-button>Start export</x-primary-button>
+        <x-secondary-button>Cancel</x-secondary-button>
+    </div>
+</div>
