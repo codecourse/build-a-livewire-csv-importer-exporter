2252bc0b3e9e393751874da0926cab3b89d76dcf
Alex Garrett-Smith <alex@codecourse.com>
2025-03-05 10:37:25 +0000

    Episode 15

--- app/Livewire/ImportModal.php
+++ app/Livewire/ImportModal.php
@@ -2,11 +2,33 @@
 
 namespace App\Livewire;
 
+use Livewire\Attributes\Computed;
+use Livewire\Attributes\Validate;
+use Livewire\WithFileUploads;
 use LivewireUI\Modal\ModalComponent;
 
 class ImportModal extends ModalComponent
 {
+    use WithFileUploads;
+
     public string $model;
+
+    #[Validate('required|mimes:csv|max:10240')]
+    public $file;
+
+    #[Computed()]
+    public function recordCount(): int
+    {
+        $file = new \SplFileObject($this->file->getRealPath(), 'r');
+        $file->seek(PHP_INT_MAX);
+
+        return $file->key() - 1;
+    }
+
+    public function startImport()
+    {
+        dd($this->file);
+    }
 
     public function render()
     {
--- resources/views/livewire/import-modal.blade.php
+++ resources/views/livewire/import-modal.blade.php
@@ -1,20 +1,24 @@
+@php use Illuminate\Support\Number; @endphp
 <div class="p-8">
     <div>
         <div class="mt-3 text-center sm:mt-0 sm:text-left">
             <h3 class="text-base font-semibold text-gray-900" id="modal-title">Import records</h3>
 
-            <!-- <div class="mt-2">
-                <p class="text-sm text-gray-500">You're about to import <strong>x</strong> records.</p>
-            </div> -->
+            @if($file)
+                <div class="mt-2">
+                    <p class="text-sm text-gray-500">You're about to import <strong>{{ Number::format($this->recordCount) }}</strong> {{ str()->plural('record', $this->recordCount) }}.</p>
+                </div>
+            @endif
 
-            <div class="mt-2">
+            @unless($file)
+                <div class="mt-2">
                 <label for="file" class="sr-only">CSV file</label>
                 <div class="mt-2 flex justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10">
                     <div class="text-center">
                         <div class="flex text-sm/6 text-gray-600">
                             <label for="file" class="relative cursor-pointer rounded-md font-semibold text-indigo-600">
                                 <span>Upload a file</span>
-                                <input id="file" name="file" type="file" class="sr-only" accept=".csv">
+                                <input id="file" name="file" type="file" class="sr-only" accept=".csv" wire:model="file">
                             </label>
                             <p class="pl-1">or drag and drop</p>
                         </div>
@@ -22,12 +26,15 @@
                     </div>
                 </div>
             </div>
+            @endunless
 
         </div>
     </div>
 
     <div class="mt-5 sm:mt-4 sm:flex sm:flex-row-reverse gap-2">
-        <x-primary-button>Start import</x-primary-button>
-        <x-secondary-button>Cancel</x-secondary-button>
+        @if($file)
+            <x-primary-button wire:click="startImport">Start import</x-primary-button>
+        @endif
+        <x-secondary-button wire:click="dispatch('closeModal')">Cancel</x-secondary-button>
     </div>
 </div>
